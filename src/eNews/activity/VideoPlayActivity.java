06d36a7f8e86c38f26a617b136ed7e7486157baf
wk415190639 <13478006132@sina.cn>
2016-09-10 18:27:07 +0800

    优化视频播放功能

--- src/eNews/activity/VideoPlayActivity.java
+++ src/eNews/activity/VideoPlayActivity.java
@@ -1,15 +1,19 @@
 package eNews.activity;
 
-import java.util.ArrayList;
-
 import io.vov.vitamio.MediaPlayer;
 import io.vov.vitamio.widget.MediaController;
 import io.vov.vitamio.widget.VideoView;
+
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import eNews.app.R;
@@ -33,10 +37,15 @@ public class VideoPlayActivity extends Activity implements OnClickListener {
 			return;
 		setContentView(R.layout.videoview);
 		getActionBar().hide();
+		int flag = WindowManager.LayoutParams.FLAG_FULLSCREEN;
+		// 获得当前窗体对象
+		Window window = getWindow();
+		// 设置当前窗体为全屏显示
+		window.setFlags(flag, flag);
 		morePopupWindow = new MorePopupWindow(this);
 		actionbar_more = (ImageButton) findViewById(R.id.actionbar_more);
 		actionbar_more.setOnClickListener(this);
-
+		backBtn = (ImageButton) findViewById(R.id.backBtn);
 		backBtn.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -55,12 +64,13 @@ public class VideoPlayActivity extends Activity implements OnClickListener {
 		mVideoView.setVideoPath(videoModel.getMp4_url());
 		mVideoView.setVideoQuality(MediaPlayer.VIDEOQUALITY_HIGH);
 		mVideoView.setMediaController(new MediaController(this));
+
 	}
 
 	@Override
 	public void onConfigurationChanged(Configuration newConfig) {
-		if (mVideoView != null)
-			mVideoView.setVideoLayout(VideoView.VIDEO_LAYOUT_SCALE, 0);
+		// if (mVideoView != null)
+		// mVideoView.setVideoLayout(VideoView.VIDEO_LAYOUT_SCALE, 0);
 		super.onConfigurationChanged(newConfig);
 	}
 
